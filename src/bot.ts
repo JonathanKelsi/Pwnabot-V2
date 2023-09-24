e9556b6c7522bb111816683eb10adf522cd21e4f
JonathanKelsi <jonathan.kelsi@gmail.com>
2023-09-24 12:19:09 +0300

    seperated logic and business completely, and tidied up a bit.

--- src/bot.ts
+++ src/bot.ts
@@ -15,19 +15,18 @@ function sanityCheck(message: Message) {
     if (message.author.bot) return false;
     if (message.channelId != process.env.CHANNEL_ID) return false;
     return message.content.startsWith(process.env.PREFIX!);
-
 }
 
 export async function bot (message: Message) {
-    if (!sanityCheck(message)) return;
+    if (!sanityCheck(message)) {
+        return;
+    }
 
     const args = message.content.slice(process.env.PREFIX!.length).trim().split(' ');
 
     if (!(args[0] in commands)) {
-        message.channel.send("Command not found");
-        return;
+        return "Command not found";
     }
 
-    const res = await commands[args[0]](args.slice(1));
-    message.channel.send(res);
+    return await commands[args[0]](args.slice(1));
 }
